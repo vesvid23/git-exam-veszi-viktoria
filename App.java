dee52ea4da8f9f612a0664d48877772f7d2bf74b
vesvid23 <vesvid23@htl-kaindorf.at>
2026-05-05 11:07:53 +0200

    modify App.java

--- App.java
+++ App.java
@@ -1,1 +1,6 @@
-System.out.println("System Initialized");
+public class App {
+    public static void main (String[] args) {
+        System.out.println("System Initialized");
+        System.out.println("Developer: Viktoria Veszi");
+    }
+}
